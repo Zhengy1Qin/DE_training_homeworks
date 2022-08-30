878d0a6864e90236bef8b96919facb0f3ec01c8e
Zhengyi Qin <zhengyi.qin@Zhengyis-MacBook-Pro-2.local>
2022-08-30 20:57:59 +0800

    [Zhengyi]add empty sql file

---
+++ examples/dw_tables_sqls.py
@@ -0,0 +1,31 @@
+default_end_time = '2999-12-31 23:59:59'
+
+dw_product_description = """
+"""
+
+dw_product_model_product_description = """
+"""
+
+dw_product_category = """
+"""
+
+dw_product_model = """
+"""
+
+dw_product = """
+"""
+
+dw_sales_order_line_item = """
+"""
+
+dw_sales_order = """
+"""
+
+dw_address = """
+"""
+
+dw_customer = """
+"""
+
+dw_customer_address = """
+"""
